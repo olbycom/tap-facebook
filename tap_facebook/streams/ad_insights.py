0d001e34b2bbe84361ead80db7aab91d3b751362
Pedro Ceriotti <pedro@nekt.com>
2026-06-15 08:48:11 -0300

    feat: Add video_thruplay action column on adinsights stream

--- tap_facebook/streams/ad_insights.py
+++ tap_facebook/streams/ad_insights.py
@@ -154,7 +154,7 @@ EXCLUDED_FIELDS = [
     "video_play_retention_0_to_15s_actions",
     "video_play_retention_20_to_60s_actions",
     "video_play_retention_graph_actions",
-    "video_thruplay_watched_actions",
+    # "video_thruplay_watched_actions",
     "video_time_watched_actions",
     "website_ctr",
     "website_purchase_roas",
